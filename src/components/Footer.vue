56bf182bbf4c82ca83b153ea7714ab47dc952be2
Jayder França <jayderfranca@gmail.com>
2018-04-22 17:20:07 -0300

    Updated site and name of developed by

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -2,7 +2,7 @@
   <div class="slim-footer">
     <div class="container">
       <p>Copyright 2018 &copy; All Rights Reserved. Nióbio Cash</p>
-      <p>Developed by: <a href="http://www.kernelit.com.br" target="_blank">Kernel IT</a></p>
+      <p>Developed by: <a href="https://kernelits.com.br" target="_blank">Kernel IT Solutions</a></p>
     </div>
   </div>
 </template>
